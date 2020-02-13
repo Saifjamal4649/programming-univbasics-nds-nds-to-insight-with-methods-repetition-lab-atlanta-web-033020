9aaaa9096e64f0e5ca216d705b80c2fb476199a3
Saif Jamal <saifjamal4649@gmail.com>
2020-02-13 04:10:44 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -37,15 +37,7 @@ director_index = 0
 end
 
 def total_gross(source)
-  # Write this implementation
-  #
-  # Should use methods:
-  # 1. directors_totals: returns a Hash of { dir_name => gross }
-  # 2. list_of_directors: names provides an Array of directors names (use
-  #
-  # Visit each key (i.e. director name), look up the value in the hash
-  # returned by directors_totals, and add it to a running total. When done,
-  # return the total
+  
   total_gross_of_earnings = 0
   director_index = 0
 
